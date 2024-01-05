2396d5498fd9f3c65872319913a26d824f914955
MUNYYBY <hm.muneeb.u.r@gmail.com>
2024-01-05 17:08:03 +0500

    chore: setup global configs for enviroments

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,17 +4,23 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
-import { jwtConstants } from './constants/constants';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('auth.secret'),
+          signOptions: { expiresIn: configService.get<string>('auth.') },
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RequestMethod, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from './config/config.type';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api', {
-    exclude: [{ path: '/', method: RequestMethod.ALL }],
-  });
+  const configService = app.get(ConfigService<AllConfigType>);
+
+  app.setGlobalPrefix(
+    configService.getOrThrow('app.apiPrefix', { infer: true }),
+    {
+      exclude: [{ path: '/', method: RequestMethod.ALL }],
+    },
+  );
   app.enableVersioning({
     type: VersioningType.URI,
   });
